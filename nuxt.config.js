ab6509c83e4ae06c574021a1102f2719dbf319b0
Balintataw J <balintataw2@gmail.com>
2020-03-21 14:37:12 -0700

    Cleanup some initial unused nuxt files

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
       }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png?v2' },
       {
         rel: 'stylesheet',
         href: 'https://fonts.googleapis.com/css?family=Material+Icons'
